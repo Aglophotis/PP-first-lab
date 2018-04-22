1da6ce90743a301d750fbcd5eeb6666bfb2703c7
Luna-PC\Luna <Atisphatis@gmail.com>
2018-04-22 19:14:29 +0300

    Revert "Push first lab"
    This reverts commit 9467009efc00e4de02899e4abf07cc927e202602.

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Calc_Test.testSum();
-        Calc_Test.testMul();
-        Calc_Test.testSub();
-        Calc_Test.testDiv();
-    }
-}
